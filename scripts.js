3f0866c23a2e7a3b10783dc3fa7b79578c12ff0e
Tyronee21 <144812005+Tyronee21@users.noreply.github.com>
2025-04-30 00:40:59 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,3 +15,22 @@ function redirectUser() {
         alert("How much do you wish for a phone?");
     }
 }
+
+(async () => {
+        const where = encodeURIComponent(JSON.stringify({
+          "Cell_Phone_Brand": {
+            "$exists": true
+          }
+        }));
+        const response = await fetch(
+          `https://parseapi.back4app.com/classes/Cell_Phone_Models_By_Brand?count=1&limit=1000&order=Cell_Phone_Brand&where=${where}`,
+          {
+            headers: {
+              'X-Parse-Application-Id': 'MEqvn3N742oOXsF33z6BFeezRkW8zXXh4nIwOQUT', // This is the fake app's application id
+              'X-Parse-Master-Key': 'uZ1r1iHnOQr5K4WggIibVczBZSPpWfYbSRpD6INw', // This is the fake app's readonly master key
+            }
+          }
+        );
+        const data = await response.json(); // Here you have the data that you need
+        console.log(JSON.stringify(data, null, 2));
+      })();
